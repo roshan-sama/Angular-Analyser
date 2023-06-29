7b01fa50140eadbe36b443ce3f078cb3a64fdb7e
Roshan Manuel <31125563+Roesh@users.noreply.github.com>
2023-06-29 04:25:54 +0000

    feat: add ability to switch tree graph direction

--- ng-analysis/src/app/app.module.ts
+++ ng-analysis/src/app/app.module.ts
@@ -8,14 +8,17 @@ import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NodeFilterPresentationComponent } from './node-filter-presentation/node-filter-presentation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
-import {MatCardModule} from '@angular/material/card'; 
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { GraphDisplaySmartComponentComponent } from './components/graph-display-smart-component/graph-display-smart-component.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { LinkFilterPresentationComponent } from './link-filter-presentation/link-filter-presentation.component';
 import { NodeDetailsPresentationComponent } from './node-details-presentation/node-details-presentation.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     NodeDetailsPresentationComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -35,7 +39,8 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatCardModule,
     MatExpansionModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-analysis/src/app/node-details-presentation/node-details-presentation.component.ts
+++ ng-analysis/src/app/node-details-presentation/node-details-presentation.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, SimpleChanges } from '@angular/core';
 import { falcorDependencyGraph } from '../interfaces/falcor-dependency-graph';
 import { ITreeGraph } from '../interfaces/tree-graph';
 import { createTreeGraphSvg } from 'src/utils/tree-graph';
+import { IFormMappingTypeHelper } from '../interfaces/form-mapping-type-helper';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'node-details-presentation',
@@ -12,34 +15,52 @@ export class NodeDetailsPresentationComponent {
   @Input() nodeDetails?: { details: falcorDependencyGraph["nodesById"][0], id: string }
   @Input() currentGraph?: falcorDependencyGraph
 
-  maxDepth = 10
+  nodeDetailsFormSubscription?: Subscription
+
+  nodeDetailsForm = new FormGroup<INodeDetailsOptionsForm>({
+    maxDepth: new FormControl(10, { nonNullable: true }),
+    treeGraphDirection: new FormControl('sourceToTarget', { nonNullable: true }),
+  })
 
   ngOnChanges(changes: SimpleChanges) {
+    this.ensureSubscription()
+
     const newNodeDetails = changes["nodeDetails"]?.currentValue
 
     if (newNodeDetails !== undefined && this.currentGraph !== undefined) {
-      const graph = this.getTreeGraphForNode(newNodeDetails, this.currentGraph)
+      const graph = this.getTreeGraphForNode(newNodeDetails, this.currentGraph, this.nodeDetailsForm.getRawValue())
       this.drawTreeGraph(graph)
     }
 
     const newCurrentGraph = changes["currentGraph"]?.currentValue
 
     if (newCurrentGraph !== undefined && this.nodeDetails !== undefined) {
-      const graph = this.getTreeGraphForNode(this.nodeDetails, newCurrentGraph)
+      const graph = this.getTreeGraphForNode(this.nodeDetails, newCurrentGraph, this.nodeDetailsForm.getRawValue())
       this.drawTreeGraph(graph)
+    }
+  }
+
+  ensureSubscription() {
+    if (!this.nodeDetailsFormSubscription) {
+      this.nodeDetailsFormSubscription = this.nodeDetailsForm.valueChanges.subscribe(value => {
+        if (this.nodeDetails !== undefined && this.currentGraph !== undefined) {
+          const graph = this.getTreeGraphForNode(this.nodeDetails, this.currentGraph, this.nodeDetailsForm.getRawValue())
+          this.drawTreeGraph(graph)
+        }
+      })
     }
   }
 
   visitedIds = new Set<string>()
 
-  getTreeGraphForNode(newNodeDetails: { details: falcorDependencyGraph["nodesById"][0], id: string }, currentGraph: falcorDependencyGraph) {
+  getTreeGraphForNode(newNodeDetails: { details: falcorDependencyGraph["nodesById"][0], id: string }, currentGraph: falcorDependencyGraph, graphOptions: INodeDetailsOptions) {
     let currentDepth = 1
     this.visitedIds.clear()
-    return this.getTreeGraphForNodeRecursive(newNodeDetails, currentGraph, currentDepth)
+    return this.getTreeGraphForNodeRecursive(newNodeDetails, currentGraph, currentDepth, graphOptions)
   }
 
   //https://codepen.io/blackjacques/pen/ZPMpza
-  getTreeGraphForNodeRecursive(newNodeDetails: { details: falcorDependencyGraph["nodesById"][0], id: string }, currentGraph: falcorDependencyGraph, currentDepth: number): ITreeGraph {
+  getTreeGraphForNodeRecursive(newNodeDetails: { details: falcorDependencyGraph["nodesById"][0], id: string }, currentGraph: falcorDependencyGraph, currentDepth: number, graphOptions: INodeDetailsOptions): ITreeGraph {
 
     const graph: ITreeGraph = {
       name: newNodeDetails.details["Name"],
@@ -51,28 +72,42 @@ export class NodeDetailsPresentationComponent {
     this.visitedIds.add(newNodeDetails.id)
 
     currentDepth++
+    if (this.nodeDetailsForm.value.maxDepth && (currentDepth > this.nodeDetailsForm.value.maxDepth)) {
+      return graph
+    }
 
-    const links = currentGraph.links.filter(link => link.source.value[1] === newNodeDetails.id)
+    const links = currentGraph.links.filter(link => {
+      if (graphOptions.treeGraphDirection === 'sourceToTarget') {
+        return link.source.value[1] === newNodeDetails.id
+      } else {
+        return link.target.value[1] === newNodeDetails.id
+      }
+    })
 
     const children = new Set<string>()
 
     links.forEach(link => {
-      const targetId = link.target.value[1]
-      children.add(targetId)
+      if (graphOptions.treeGraphDirection === 'sourceToTarget') {
+        const targetId = link.target.value[1]
+        children.add(targetId)
+      } else {
+        const targetId = link.source.value[1]
+        children.add(targetId)
+      }
     })
 
-    if(children.size > 0) {
+    if (children.size > 0) {
       graph.children = []
     }
 
     children.forEach(childId => {
-      if(!this.visitedIds.has(childId)) {
+      if (!this.visitedIds.has(childId)) {
         const childNodeDetails = {
           details: currentGraph.nodesById[childId],
           id: childId
         }
 
-        const childGraph = this.getTreeGraphForNodeRecursive(childNodeDetails, currentGraph, currentDepth)
+        const childGraph = this.getTreeGraphForNodeRecursive(childNodeDetails, currentGraph, currentDepth, graphOptions)
 
         graph.children?.push(childGraph)
       }
@@ -93,4 +128,13 @@ export class NodeDetailsPresentationComponent {
       console.error(`chartDiv element wasn't selectable`)
     }
   }
+}
+
+interface INodeDetailsOptions {
+  maxDepth: number,
+  treeGraphDirection: 'sourceToTarget' | 'targetToSource'
+}
+
+interface INodeDetailsOptionsForm extends IFormMappingTypeHelper<INodeDetailsOptions> {
+
 }
